e3a5dd24397bde6bf11be91fc7ea77ebba335ef1
278deco <278deco.officiel@gmail.com>
2023-03-08 14:55:30 +0100

    Created new storing object named Tuple used to store short values

---
+++ src/main/java/fr/eseoye/eseoye/utils/Tuple.java
@@ -0,0 +1,36 @@
+package fr.eseoye.eseoye.utils;
+
+public class Tuple<A,B> {
+	
+	private A valueA;
+	private B valueB;
+	
+	public Tuple(A a, B b) {
+		this.valueA = a;
+		this.valueB = b;
+	}
+	
+	public A getValueA() {
+		return valueA;
+	}
+	
+	public B getValueB() {
+		return valueB;
+	}
+	
+	@Override
+	public String toString() {
+		return "("+valueA+", "+valueB+")";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof Tuple && equal((Tuple<?,?>)obj);
+	}
+	
+	private boolean equal(Tuple<?, ?> tuple) {
+		return tuple.getValueA().equals(valueA) &&
+			tuple.getValueB().equals(valueB);
+	}
+	
+}
